7447177cd62c7afaea7b0975084d19ad5ed7d3b7
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-11 22:20:25 +0200

    made the code more readable

--- main.py
+++ main.py
@@ -14,17 +14,25 @@ TO_BOTTOM_LEFT_OFFSET = Y_DIAGONAL_OFFSET - X_DIAGONAL_OFFSET
 TO_TOP_RIGHT_OFFSET = -Y_DIAGONAL_OFFSET + X_DIAGONAL_OFFSET
 TO_TOP_LEFT_OFFSET = -Y_DIAGONAL_OFFSET - X_DIAGONAL_OFFSET
 
+# store in a dictionnary all the loosing positions
 loosing_hashtable = {}
+# store in a list all the solutions found
 solutions = []
 
 
+# a subgrid is of a square 'a' is the set of all the squares that 'a' can go to with only horizontal and vertical moves
+# e.g. in normal setting the subgrid of the square of index 0 is composed of the squares of indexes
+#  0,  3,  6,  9
+# 30, 33, 36, 39
+# 60, 63, 66, 69
+# 90, 93, 96, 99
 def is_subgrid_horizontal_line_filled(grid: int, index: int) -> bool:
-    y = index - index % WIDTH
-    x = index % WIDTH % HORIZONTAL_OFFSET
-    while x < WIDTH:
-        if not grid & 1 << (y + x):
+    y_offset = index - index % WIDTH
+    default_x_offset = index % WIDTH % HORIZONTAL_OFFSET
+
+    for x_offset in range(default_x_offset, WIDTH, HORIZONTAL_OFFSET):
+        if not grid & 1 << (y_offset + x_offset):
             return False
-        x += HORIZONTAL_OFFSET
 
     return True
 
@@ -39,58 +47,54 @@ def is_sub_grid_filled(grid: int, index: int) -> bool:
     return True
 
 
+# the position/hash will only be the squares filled and the current index so that a position may be acquired by multiple path
+# but the result will be the same
+# the index is << of the size of the grid so that they don't superpose
+# e.g. on a 2x2 grid with an index of 1
+# hash =
+#        0010 (grid 0b0010)
+#    00010000 (index 1 or 0b0001)
+#    00010010 (result)
 def get_hash(grid: int, index: int) -> int:
-    return grid | index << 100
+    return grid | index << DIGITS_NUMBER
 
 
 def show_grid(played_moves: list[str]) -> str:
-    positions = {}
+    # played_moves: turn -> move (index)
+    # squares: index -> turn ("1" -> "100")
+    squares = {}
     for i in range(len(played_moves)):
-        positions[played_moves[i]] = str(i + 1)
+        squares[played_moves[i]] = str(i + 1)
 
     str_grid = ""
-    for i in range(HEIGHT):
-        str_line = ""
-        for j in range(WIDTH):
-            move = positions.get(str(i * WIDTH + j)) or "0"
-            if len(move) == 1:
-                move = "  " + move
-            elif len(move) == 2:
-                move = " " + move
-            str_line += " " + move + " "
-        str_grid += str_line + "\n"
+    for index in range(DIGITS_NUMBER):
+        # get the turn the square has been filled
+        move = squares.get(str(index)) or "0"
+
+        # add space so that once the grid is printed, columns align nicely
+        if len(move) == 1:
+            move = "  " + move
+        elif len(move) == 2:
+            move = " " + move
+        str_grid += " " + move + " "
+
+        # end of the row
+        if index % WIDTH == WIDTH - 1:
+            str_grid += "\n"
+
     return str_grid
 
 
-def get_moves(grid: int, index: int, unperfect=False) -> list[int]:
+def get_moves(grid: int, index: int) -> list[int]:
+    # all the index that are available to go to
     indexes = []
 
-    sub_grid_filled = not unperfect or is_sub_grid_filled(grid, index)
+    sub_grid_filled = is_sub_grid_filled(grid, index)
 
-    if not unperfect or not sub_grid_filled:
-        # right
-        if (
-            index + HORIZONTAL_OFFSET < DIGITS_NUMBER
-            and index % WIDTH < 7
-            and not grid & 1 << (index + HORIZONTAL_OFFSET)
-        ):
-            indexes.append(index + HORIZONTAL_OFFSET)
-        # left
-        if (
-            index - HORIZONTAL_OFFSET >= 0
-            and index % WIDTH > 2
-            and not grid & 1 << (index - HORIZONTAL_OFFSET)
-        ):
-            indexes.append(index - HORIZONTAL_OFFSET)
-        # down
-        if index + VERTICAL_OFFSET < DIGITS_NUMBER and not grid & 1 << (
-            index + VERTICAL_OFFSET
-        ):
-            indexes.append(index + VERTICAL_OFFSET)
-        # up
-        if index - VERTICAL_OFFSET >= 0 and not grid & 1 << (index - VERTICAL_OFFSET):
-            indexes.append(index - VERTICAL_OFFSET)
-    if not unperfect or sub_grid_filled:
+    # wait until the subgrid is filled to move to the next subgrid
+    # it might prune some solutions but it allow to really fastly find 60 solutions
+    # return horizontal and vertical if subgrid not filled and diagonal otherwise
+    if sub_grid_filled:
         if (
             index + TO_BOTTOM_RIGHT_OFFSET < DIGITS_NUMBER
             and index % WIDTH < 8
@@ -115,29 +119,56 @@ def get_moves(grid: int, index: int, unperfect=False) -> list[int]:
             and not grid & 1 << (index - TO_TOP_RIGHT_OFFSET)
         ):
             indexes.append(index - TO_TOP_RIGHT_OFFSET)
+    else:
+        # right
+        if (
+            index + HORIZONTAL_OFFSET < DIGITS_NUMBER
+            and index % WIDTH < 7
+            and not grid & 1 << (index + HORIZONTAL_OFFSET)
+        ):
+            indexes.append(index + HORIZONTAL_OFFSET)
+        # left
+        if (
+            index - HORIZONTAL_OFFSET >= 0
+            and index % WIDTH > 2
+            and not grid & 1 << (index - HORIZONTAL_OFFSET)
+        ):
+            indexes.append(index - HORIZONTAL_OFFSET)
+        # down
+        if index + VERTICAL_OFFSET < DIGITS_NUMBER and not grid & 1 << (
+            index + VERTICAL_OFFSET
+        ):
+            indexes.append(index + VERTICAL_OFFSET)
+        # up
+        if index - VERTICAL_OFFSET >= 0 and not grid & 1 << (index - VERTICAL_OFFSET):
+            indexes.append(index - VERTICAL_OFFSET)
 
     return indexes
 
 
-def dfs(grid: int, index: int, played_moves: list[str], depth: int = 2) -> bool:
+def solve(grid: int, index: int, played_moves: list[str], depth: int = 2) -> bool:
     global loosing_hashtable, solutions
 
-    if depth == 100 + 1:
+    # if grid is full
+    if depth == DIGITS_NUMBER + 1:
         solutions.append(played_moves.copy())
         return True
 
-    for move in get_moves(grid, index, unperfect=True):
+    for move in get_moves(grid, index):
+        # make the move
         grid |= 1 << move
         played_moves.append(str(move))
 
         result = False
+        # go deeper if the position doesn't match any previous loosing position explored
         if not loosing_hashtable.get(get_hash(grid, move)):
-            result = dfs(grid, move, played_moves, depth + 1)
+            result = solve(grid, move, played_moves, depth + 1)
 
         # save the position as loosing
         if not result:
             loosing_hashtable[get_hash(grid, move)] = (grid, move)
 
+        # cancel the move
         played_moves.pop(-1)
         grid ^= 1 << move
 
@@ -145,7 +176,7 @@ def dfs(grid: int, index: int, played_moves: list[str], depth: int = 2) -> bool:
 
 
 if __name__ == "__main__":
-    dfs(1, 0, ["0"])
+    solve(1, 0, ["0"])
     text = f"-- all solutions ({len(solutions)}) --\n"
     for i in range(len(solutions)):
         text += f"# solution {i+1}:\n"
