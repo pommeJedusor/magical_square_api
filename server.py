b62d54f69d6ec3d21977618db610a7d9c7ae17e8
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-12-15 20:37:21 +0100

    fix cors policy

--- server.py
+++ server.py
@@ -49,6 +49,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             print(len(moves))
             self.send_response(200)
             self.send_header("Content-type", "application/json")
+            self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
             self.wfile.write(json.dumps(moves).encode())
         except:
