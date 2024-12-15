89f48fd5a826c3f3a7e07a94cd8013f5ff8ed00f
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-12-15 17:22:40 +0100

    basic api

---
+++ sever.py
@@ -0,0 +1,58 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+from pyArango.connection import *
+from pyArango.collection import Collection, Field
+
+conn = Connection(username="root", password="root")
+db_name = "magical_square"
+
+if db_name not in conn:
+    db = conn.createDatabase(name=db_name)
+else:
+    db = conn[db_name]
+
+
+class Nodes(Collection):
+    _fields = {"hash": Field()}
+
+
+class Edges(Collection):
+    _fields = {"from_index": Field(), "to_index": Field()}
+
+
+nodes = db["Nodes"]
+edges = db["Edges"]
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            hash = int(self.path[1:])
+            query = f"""
+            FOR edge IN Edges
+                FILTER edge._from == 'Nodes/{hash}'
+                RETURN edge.to_index
+            """
+            cursor = db.AQLQuery(query, rawResults=True)
+            moves = list(cursor)
+
+            print(len(moves))
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(moves).encode())
+        except:
+            self.send_response(404)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"error": "Hash not found"}).encode())
+
+
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
+    server_address = ("", port)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
